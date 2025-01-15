e38a0fba15f42f59479f5fd06ee91741d5e88360
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2025-01-15 13:10:52 +0100

    feat: save generated rdf and measured metrics

--- fiware/kgcp/fiware_kgcp.py
+++ fiware/kgcp/fiware_kgcp.py
@@ -1,3 +1,4 @@
+import json
 from iot2kg import RDFGenerator
 from settings.config import project_root_path
 import time
@@ -35,33 +36,42 @@ def measure_performance(kg_gen, repetitions=20):
     return mem_usage, elapsed_time
 
 
-metrics = dict()
+if __name__ == '__main__':
 
-for hotel in (
-        "hotel_aachen_004_hotel_100rooms",
-        "hotel_aachen_003_hotel_50rooms",
-        "hotel_aachen_002_hotel_10rooms",
-        "hotel_aachen_001_hotel_2rooms"):
-    kg_generator = RDFGenerator(
-        mapping_file=f"{project_root_path}/fiware/kgcp/rml/fiware_hotel_rml.ttl",
-        source_file=f"{project_root_path}/fiware/hotel_dataset/{hotel}.json",
-        destination_file=f"{project_root_path}/fiware/kgcp/results/{hotel}.ttl",
-        engine="morph-kgc"
-    )
+    metrics = dict()
 
-    repeat = 20
-    m_usage, time_usage = measure_performance(kg_generator,
-                                              repetitions=repeat)
+    for hotel in (
+            "fiware_entities_2rooms",
+            "fiware_entities_10rooms",
+            "fiware_entities_50rooms",
+            "fiware_entities_100rooms",
+            "fiware_entities_500rooms",
+            "fiware_entities_1000rooms"
+    ):
+        kg_generator = RDFGenerator(
+            mapping_file=f"{project_root_path}/fiware/kgcp/rml/fiware_hotel_rml.ttl",
+            source_file=f"{project_root_path}/fiware/hotel_dataset/{hotel}.json",
+            destination_file=f"{project_root_path}/fiware/kgcp/results/{hotel}.ttl",
+            engine="morph-kgc"
+        )
 
-    print(f"memory usage for {hotel} in MiB")
-    print(f"Average: {sum(m_usage)/len(m_usage)}")
-    print(f"Max: {max(m_usage)}")
-    print(f"Min: {min(m_usage)}")
+        repeat = 20
+        m_usage, time_usage = measure_performance(kg_generator,
+                                                  repetitions=repeat)
 
-    print(f"Time usage for {hotel} in second")
-    print(f"Total: {time_usage}")
-    print(f"Average: {time_usage/repeat}")
+        print(f"memory usage for {hotel} in MiB")
+        print(f"Average: {sum(m_usage)/len(m_usage)}")
+        print(f"Max: {max(m_usage)}")
+        print(f"Min: {min(m_usage)}")
 
-    metrics[hotel] = {"memory": m_usage,
-                      "time": time_usage,
-                      "repetitions": repeat}
+        print(f"Time usage for {hotel} in second")
+        print(f"Total: {time_usage}")
+        print(f"Average: {time_usage/repeat}")
+
+        metrics[hotel] = {"memory": m_usage,
+                          "time": time_usage,
+                          "repetitions": repeat}
+        # save metrics as JSON file
+        with open(f"{project_root_path}/fiware/kgcp/results/metrics.json", "w") as f:
+            json.dump(metrics, f, indent=2)
+
